2afd7c39b4c0790de560d4b771352b741a123f0b
xudoyshukur <nazirboyevxudoyshukur3@gmail.com>
2026-02-28 16:45:06 +0500

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <>
             <nav>
-                <div id="logo">Abbos</div>
+                <div id="logo">Xudoyshukur</div>
                 <ul>
                     <li>
                         <Link to={"/"}>Home</Link>
